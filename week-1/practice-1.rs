75585f7642a7ffb629bb04eaf17f0c5d85946ae1
j.emmanuelCOS101 <jhae440@gmail.com>
2023-10-13 11:49:05 +0100

    (My first commit)

---
+++ week-1/practice-1.rs
@@ -0,0 +1,13 @@
+fn main(){
+	println!("Welcome to COS 101")
+	
+	println!("the course outcome is as follows:")
+
+	println!("1.distingush between computer and computer programming,/n
+	          2.develop some techniques in computer science,/n
+	          3.understand the different areas of studyin computer science,/n
+	          4.conversant with applications of computer science,/n
+	          5.navigate the career prospects in computer science,/n
+	          6.conversant with computer programming concepts.")
+
+}
