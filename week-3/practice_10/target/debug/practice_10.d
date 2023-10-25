ae64d3df82550cf45f428613f2d6852071ad8cef
j.emmanuelCOS101 <jhae440@gmail.com>
2023-10-25 23:22:12 +0100

    (week-3)

---
+++ week-3/practice_10/target/debug/practice_10.d
@@ -0,0 +1,1 @@
+C:\Users\JOHN\ PAUL\Documents\j.emmanuelCOS101\week-3\practice_10\target\debug\practice_10.exe: C:\Users\JOHN\ PAUL\Documents\j.emmanuelCOS101\week-3\practice_10\src\main.rs
---
+++ week-3/practice_6/src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    let intrest:f32 = 8.0;   // integer assigned to float
+    println!("intrest is {}",intrest);
+}
